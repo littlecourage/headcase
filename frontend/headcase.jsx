880548dd3f70ea2a37ec9eb42086c715e475ecdb
littlecourage <christine.adams21@gmail.com>
2020-11-17 09:51:26 -0500

    Created store

--- frontend/headcase.jsx
+++ frontend/headcase.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from './components/root';
+import configureStore from "./store/store";
 
 
 document.addEventListener("DOMContentLoaded", () => {
 
   const root = document.getElementById("root");
+  const store = configureStore();
   ReactDOM.render(<h1>Welcome to headcase</h1>, root);
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 }); 
