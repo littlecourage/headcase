c902f93b2ec848e0b9ece739a0c9cd1c1578d9ee
littlecourage <christine.adams21@gmail.com>
2020-11-19 20:13:52 -0500

    Adding packs to user's dashboard

--- frontend/components/user_dash/user_dash.jsx
+++ frontend/components/user_dash/user_dash.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 
 class UserDash extends React.Component {
@@ -9,35 +10,45 @@ class UserDash extends React.Component {
 
 
   render() {
-
-    return (
-      <div className="dashHome">
-        <div className="dashBackground">
-          <img src={window.userDashBackgroundUrl} />
-        </div>
-        <div className="rectangles">
-          <div className="greyRectangle"></div>
-          <div className="transparentRectangle"></div>
-        </div>
-        <div className="packsTitle">
-          <h1>My packs</h1>
-          <span>Themed meditations for specific topics. Each session</span>
-          <span>builds on the one before it</span>
-        </div>
-        <div className="packBox">
-          <div className="pack">
-            Balance
+    if (!this.props.currentUser) {
+      return (
+        <Redirect to="/" />
+      )
+    } else {
+      return (
+        <div className="dashHome">
+          <div className="dashBackground">
+            <img src={window.userDashBackgroundUrl} />
           </div>
-          <div className="pack">
-            Happiness
+  
+          <div className="rectangles">
+            <div className="greyRectangle"></div>
+            <div className="transparentRectangle"></div>
           </div>
-          <div className="pack">
-            Managing Anxiety
+  
+          <div className="packsTitle">
+            <h1>My packs</h1>
+            <span>Themed meditations for specific topics. Each session</span>
+            <span>builds on the one before it</span>
           </div>
+  
+          <div className="packBox">
+            <div className="pack">
+              <img src={window.packYellow1} />
+            </div>
+            <div className="pack">
+              <img src={window.packTeal1} />
+            </div>
+            <div className="pack">
+              <img src={window.packGray1} />
+            </div>
+          </div>
+  
+          <button className="show_button">SHOW MORE</button>
+  
         </div>
-
-      </div>
-    )
+      )
+    }
 
   }
 
