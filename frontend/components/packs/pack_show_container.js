e6bfbdb96f060a86eed3ee4422c8527824708237
littlecourage <christine.adams21@gmail.com>
2020-11-29 19:25:47 -0500

    styled packs show and modified user packs controller to have create method, currently editing util

--- frontend/components/packs/pack_show_container.js
+++ frontend/components/packs/pack_show_container.js
@@ -1,17 +1,27 @@
 import { connect } from 'react-redux';
 import PackShow from './pack_show_page';
 import { fetchPack } from '../../actions/packs_actions';
+import { fetchAllUserPacks } from '../../actions/user_packs_actions';
+
+// const isUserPack = (pack={}, userPacks=[]) => {
+//   let packIds = userPacks.map((userPack) => userPack.packId)
+//   return packIds.includes(pack.id)
+// }
 
 
 const mapStateToProps = (state, ownProps) => {
+  
   return {
-    pack: state.entities.packs[ownProps.match.params.packId]
+    pack: state.entities.packs[ownProps.match.params.packId],
+    userPacks: Object.values(state.entities.userPacks),
+    // isUserPack: isUserPack(pack, userPacks)
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    fetchPack: (packId) => dispatch(fetchPack(packId))
+    fetchPack: (packId) => dispatch(fetchPack(packId)),
+    fetchAllUserPacks: () => dispatch(fetchAllUserPacks())
   }
 }
 
--- frontend/components/packs/pack_show_page.jsx
+++ frontend/components/packs/pack_show_page.jsx
@@ -1,18 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { IoIosAddCircleOutline, IoIosCloseCircleOutline } from "react-icons/io";
 
 
 class PackShow extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+    console.log(this.state)
+    this.handleIsUserPack = this.handleIsUserPack.bind(this);
+  }
+
   componentDidMount() {
     this.props.fetchPack(this.props.match.params.packId);
+    this.props.fetchAllUserPacks();
+
+  }
+
+  handleIsUserPack() {
+
   }
 
   render() {
-    const { pack } = this.props;
+    const { pack, userPacks } = this.props;
     return (pack) ? (
-      <div>
-        <h2>{pack.title}</h2>
+      <div className="showContainer">
+        <div className="showText">
+          <h2>{pack.title}</h2>
+          <span className="sessions">{pack.length} SESSIONS</span>
+          
+          <p>Live happier and healthier by learning the 
+            fundamentals of meditation and mindfulness.</p>
+          
+          {userPacks
+            .map(uP => uP.packId)
+            .includes(pack.id) ? 
+            (<div className="selectOrRemove"><span className="icon"><IoIosCloseCircleOutline /></span> 
+              <span >&emsp;REMOVE FROM MY PACKS</span></div>) 
+            : (<div className="selectOrRemove"><span className="icon"><IoIosAddCircleOutline /></span> 
+              <span>&emsp;ADD TO MY PACKS</span></div>)}
+        </div>
+        <div className="showImg">
+          <img 
+            src={window.packListening} 
+            alt="illustration of someone listening to meditation"/>
+        </div>
       </div>
     ) : (
       null
