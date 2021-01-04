7534d8072517e2489260dde17c4ad5f15bd4cf64
littlecourage <christine.adams21@gmail.com>
2021-01-04 18:39:58 -0500

    Add title and day to footer

--- frontend/components/packs/pack_footer.jsx
+++ frontend/components/packs/pack_footer.jsx
@@ -5,10 +5,13 @@ import { FaPlay } from 'react-icons/fa';
 
 
  const PackFooter = ({expanded, pack, handleAdd, handleExpand, userPacks, className}) => {
-  let added =  (userPacks.map(uP => uP.packId).includes(pack.id));
+  let added = (userPacks.map(uP => uP.packId).includes(pack.id));
   let currentTrack;
+  let num = 1;
+
   if (added) {
     currentTrack = userPacks.filter(uP => uP.packId === pack.id)[0].currentMeditation.id;
+    num = userPacks.filter(uP => uP.packId === pack.id)[0].currentMeditation.order;
   } else {
     currentTrack = pack.meditations[0].id;
   }
@@ -18,6 +21,7 @@ import { FaPlay } from 'react-icons/fa';
       <img src={window.footerImg} alt="background image"/>
       <div>
         <Link to={`/play/${currentTrack}`} onClick={added ? null : handleAdd}><FaPlay />&#8239;&ensp;BEGIN</Link>
+        <span>Day {num} of {pack.title}</span>
         <span onClick={handleExpand}><IoIosArrowDropdown /></span>
       </div>
     </div>
@@ -26,6 +30,7 @@ import { FaPlay } from 'react-icons/fa';
         <img src={window.footerImg} alt="background image" />
         <div>
           <Link to={`/play/${currentTrack}`} onClick={added ? null : handleAdd}><FaPlay />&#8239;&ensp;BEGIN</Link>
+          <span>Day {num} of {pack.title}</span>
           <span onClick={handleExpand}><IoIosArrowDropdown /></span>
         </div>
     </div>
