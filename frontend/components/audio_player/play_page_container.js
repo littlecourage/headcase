d63ec1c077c53e05561c9ca1b6c9aeeb752bfbd8
littlecourage <christine.adams21@gmail.com>
2021-01-05 16:49:13 -0500

    Updated play page container so the next meditation from any pack can be played, not just the most recent one.

--- frontend/components/audio_player/play_page_container.js
+++ frontend/components/audio_player/play_page_container.js
@@ -4,15 +4,14 @@ import { fetchAllUserPacks } from '../../actions/user_packs_actions';
 import { createMeditationCompletion } from '../../actions/meditation_completions_actions';
 import PlayPage from './play_page';
 
-const mapStateToProps = (state) => {
-
-  let currentMed = state.entities.users[state.session.id].currentMeditation;
+const mapStateToProps = (state, ownProps) => {
+  let currentMedId = parseInt(ownProps.match.params.meditationId);
   let uPacks = Object.values(state.entities.userPacks);
-  let currentUp = uPacks.find(up => up.currentMeditation.id === currentMed.id);
-
+  let currentUp = uPacks.find(up => up.currentMeditation.id === currentMedId);
+  let currentMed = Object.values(currentUp.meditations).find(med => med.id === currentMedId);
   return {
     currentMed: currentMed,
-    currentTrack: state.entities.users[state.session.id].trackUrl,
+    currentTrack: currentMed.trackUrl,
     currentUp: currentUp
   }
 }
