8c49263bae4e21530dfedb25fbd43e65f69fde13
littlecourage <christine.adams21@gmail.com>
2021-01-05 00:02:51 -0500

    Add progress bar to footer that updates based on state

--- frontend/components/packs/pack_footer.jsx
+++ frontend/components/packs/pack_footer.jsx
@@ -8,30 +8,61 @@ import { FaPlay } from 'react-icons/fa';
   let added = (userPacks.map(uP => uP.packId).includes(pack.id));
   let currentTrack;
   let num = 1;
+  let progress = {
+    width: '100%',
+    height: '8px',
+  }
+  let bar = {
+    width: 0,
+    height: '8px',
+    backgroundColor: '#413D47'
+  }
 
   if (added) {
     currentTrack = userPacks.filter(uP => uP.packId === pack.id)[0].currentMeditation.id;
     num = userPacks.filter(uP => uP.packId === pack.id)[0].currentMeditation.order;
+    length = userPacks.filter(uP => uP.packId === pack.id)[0].length;
+    console.log(num);
+    console.log(length);
+    if (num > 1) {
+      bar.width = ((num - 1)/length) * 100 + "%"
+      console.log(bar.width);
+      progress.backgroundColor =  'rgba(255, 255, 255, 0.2)'
+    }
   } else {
     currentTrack = pack.meditations[0].id;
   }
   
   return (expanded) ? (
     <div className={className}>
-      <img src={window.footerImg} alt="background image"/>
-      <div>
-        <Link to={`/play/${currentTrack}`} onClick={added ? null : handleAdd}><FaPlay />&#8239;&ensp;BEGIN</Link>
-        <span>Day {num} of {pack.title}</span>
-        <span onClick={handleExpand}><IoIosArrowDropdown /></span>
+      <div className="outer">
+        <img src={window.footerImg} alt="background image"/>
+        <div className="description">
+          <div>
+            <Link to={`/play/${currentTrack}`} onClick={added ? null : handleAdd}><FaPlay />&#8239;&ensp;BEGIN</Link>
+            <span>Day {num} of {pack.title}</span>
+          </div>
+          <span onClick={handleExpand}><IoIosArrowDropdown /></span>
+        </div>
+        <div className="progress" style={progress}>
+          <div style={bar}></div>
+        </div>
       </div>
     </div>
   ) : (
     <div className={className}>
-        <img src={window.footerImg} alt="background image" />
-        <div>
-          <Link to={`/play/${currentTrack}`} onClick={added ? null : handleAdd}><FaPlay />&#8239;&ensp;BEGIN</Link>
-          <span>Day {num} of {pack.title}</span>
-          <span onClick={handleExpand}><IoIosArrowDropdown /></span>
+        <div className="outer">
+          <img src={window.footerImg} alt="background image" />
+          <div className="description">
+            <div>
+              <Link to={`/play/${currentTrack}`} onClick={added ? null : handleAdd}><FaPlay />&#8239;&ensp;BEGIN</Link>
+              <span>Day {num} of {pack.title}</span>
+            </div>
+            <span onClick={handleExpand}><IoIosArrowDropdown /></span>
+          </div>
+          <div className="progress" style={progress}>
+            <div style={bar}></div>
+          </div>
         </div>
     </div>
   )
